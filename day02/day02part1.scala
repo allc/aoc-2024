6a5d958e9524292baefa39f6f8f1d8f0e2d2502b
J Cui <i@cjxol.com>
2024-12-02 06:13:47 +0000

    Add day 2

---
+++ day02/day02part1.scala
@@ -0,0 +1,37 @@
+import scala.io.Source
+
+object Day02Part1 {
+    def checkReport(report: List[Int]): Boolean = {
+        var isIncreasing = true
+        for (i <- 1 to report.length - 1) {
+            if (i == 1) {
+                isIncreasing = report(i) > report(i - 1)
+            } else if (report(i) > report(i - 1) != isIncreasing) {
+                return false
+            }
+            if (math.abs(report(i) - report(i - 1)) < 1 || math.abs(report(i) - report(i - 1)) > 3) {
+                return false
+            }
+        }
+        true
+    }
+    
+  def main(args: Array[String]) {
+    val reports: List[List[Int]] = Source.fromFile("input.txt")
+        .getLines()
+        .map(line =>
+            line.split("\\s+")
+                .map(_.toInt)
+                .toList
+        )
+        .toList
+        
+    var results = 0
+    for (report <- reports) {
+        if (checkReport(report)) {
+            results += 1
+        }
+    }
+    println(results)
+  }
+}
